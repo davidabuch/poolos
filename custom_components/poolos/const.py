4595f62fda1af57ca8074acf58db91eefa8270be
davidabuch <davidabuch@gmail.com>
2026-09-03 17:32:17 -0700

    Bump PoolOS integration version to 0.10.1

--- custom_components/poolos/const.py
+++ custom_components/poolos/const.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 DOMAIN = "poolos"
 NAME = "PoolOS"
-INTEGRATION_VERSION = "0.10.0"
+INTEGRATION_VERSION = "0.10.1"
 
 CONF_DIAGNOSTICS_ENABLED = "diagnostics_enabled"
 DEFAULT_DIAGNOSTICS_ENABLED = True
