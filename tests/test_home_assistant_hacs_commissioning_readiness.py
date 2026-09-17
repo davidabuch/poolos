66395a9469179a42ce271ca966514e0d37777236
davidabuch <davidabuch@gmail.com>
2026-09-16 17:54:53 -0700

    Release PoolOS v0.11.26

--- tests/test_home_assistant_hacs_commissioning_readiness.py
+++ tests/test_home_assistant_hacs_commissioning_readiness.py
@@ -8,8 +8,8 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[1]
 COMPONENT = ROOT / "custom_components" / "poolos"
-EXPECTED_VERSION = "0.11.25"
-EXPECTED_CORE_REQUIREMENT = "poolos@git+https://github.com/davidabuch/poolos.git@v0.11.25"
+EXPECTED_VERSION = "0.11.26"
+EXPECTED_CORE_REQUIREMENT = "poolos@git+https://github.com/davidabuch/poolos.git@v0.11.26"
 EXPECTED_PROTOCOL_REQUIREMENT = "pyintellicenter==0.1.20"
 
 
